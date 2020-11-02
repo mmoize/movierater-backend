56079e116ee5ca664c565de83b74dbc889e4e649
Moise Murhi <murhi46@gmail.com>
2020-11-02 14:05:05 +1100

    allowed hosts

--- movierater/settings.py
+++ movierater/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost:4200',
+    'muvirater.herokuapp.com'
+]
 
 
 # Application definition
