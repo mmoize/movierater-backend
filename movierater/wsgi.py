6793dc5321c3c4133369bfe583fefb4a1f243237
Moise Murhi <murhi46@gmail.com>
2020-11-02 13:11:59 +1100

    configuring wsgi for static files upload logic

--- movierater/wsgi.py
+++ movierater/wsgi.py
@@ -10,9 +10,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movierater.settings')
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(MediaCling(get_wsgi_application()))
